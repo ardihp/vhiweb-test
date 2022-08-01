49f80fc4d3c3c3bad9f8908876d0cb00951f40f9
ardihp <ardihp8@gmail.com>
2022-08-01 14:12:09 +0700

    feat: 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,16 @@
+import History from "helpers/History";
 import React from "react";
 
 const Page404 = () => {
-  return <div>Page 404</div>;
+  return (
+    <div className="container-404">
+      <p className="title">404</p>
+      <p className="desc">Page Not Found.</p>
+      <button className="btn-white" onClick={() => History.push("/users")}>
+        Way Back Home
+      </button>
+    </div>
+  );
 };
 
 export default Page404;
